59cfaa109a8755b4551833179cbc0b9fc094790a
snibbels <malte.fillmer@googlemail.com>
2018-02-20 09:23:36 +0100

    fix select control behavior
    now works on firefox too

--- src/select_control.ts
+++ src/select_control.ts
@@ -3,9 +3,21 @@ export default class Select_Control{
 
     constructor(private command:string, values:string[], private overwrite:(value:string)=>void){
         this.element = document.createElement("select");
-        this.element.onclick = this.trigger.bind(this);
-        
+        let placeholder = document.createElement("option");
+        this.element.onclick = e => {
+            placeholder.value = placeholder.innerHTML = this.element.value + " ";
+            this.element.value = placeholder.value;
+        }
+        this.element.onchange = this.trigger.bind(this);
+        placeholder.setAttribute('disabled', 'true');
+        placeholder.setAttribute('selected', 'true');
+        this.element.appendChild(placeholder);
+
         for(let value of values){
+            if(!placeholder.value){
+                placeholder.value = placeholder.innerHTML = value + " ";
+            }
+
             let temp_element = document.createElement("option");
             temp_element.value = value;
             temp_element.innerHTML = value;
